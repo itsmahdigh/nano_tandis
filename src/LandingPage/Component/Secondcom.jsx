9b83ae1f1e60e0fd14807c37b1d0fd8e9b349915
mahdigh621 <mahdigh621@gmail.com>
2024-03-02 23:22:14 +0330

    responsive

--- src/LandingPage/Component/Secondcom.jsx
+++ src/LandingPage/Component/Secondcom.jsx
@@ -3,43 +3,43 @@ export default function SecondComp() {
   return (
     <>
       <div>
-        <div className="text-4xl font-bold mt-16  ">
-          <h1 className="text-[#21462A]">
+        <div className="font-bold mt-12  ">
+          <p className="text-[#21462A] text-3xl md:text-5xl">
             چرا باید
             <br />
             این محصولات را از ما بخرید؟!
-          </h1>
-          <p className="text-xl font-medium text-[#284630] mt-5">
+          </p>
+          <p className="text-sm md:text-lg font-medium text-[#284630] mt-5">
             حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد کتابهای زیادی در شصت و سه درصد گذشته،<br />
 
             طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص <br />
 
           </p>
         </div>
-        <div className="w-[80%] mx-auto flex">
-          <div className="w-[25%] mx-auto grid content-around">
-            <p className="text-left mt-7 text-md ">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-              چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچن</p>
-            <div className="text-left mt-7 text-md">
+        <div className="w-[90%] md:w-[75%] grid grid-cols-3 mx-auto ">
+          <div className=" mx-auto grid content-around">
+            <p className="text-left mt-7 text-sm ">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+             ن</p>
+            <div className="text-left mt-7 text-sm md:text-lg">
               <p> 
                 موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد
-                و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته
-                اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد
+                 اصلی و جوابگوی سوالات پیوسته
+               د
               </p>
             </div>
           </div>
 
-          <div className="w-[30%] mx-auto">
-            <img src={img} alt="" className=' mt-4' />
+          <div className=" grid content-center">
+            <img src={img} alt="" className=' mt-4 w[100%]' />
           </div>
-          <div className="w-[25%] mx-auto grid content-around">
+          <div className="mx-auto grid content-around">
             <div>
-              <p className="text-right mt-7 text-md ">شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری
-                را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.</p>
+              <p className="text-right mt-7 text-sm ">شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری
+               د.</p>
             </div>
             <div className="">
-              <p className="text-right mt-7 text-md ">شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری
-                را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.</p>
+              <p className="text-right mt-7 text-sm ">شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری
+                د.</p>
             </div>
           </div>
         </div>
